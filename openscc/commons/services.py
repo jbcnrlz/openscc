d0aec4fd771f9972cba1901e55d5006a3d7919e9
João Cardia <joao.cardia@fatec.sp.gov.br>
2026-05-12 10:37:15 -0300

    feat: Update LLM model to gemini-3.1-flash-lite for enhanced performance

--- openscc/commons/services.py
+++ openscc/commons/services.py
@@ -21,7 +21,7 @@ def get_llm():
             #base_url="http://192.168.86.20:11434",
         )
     return ChatGoogleGenerativeAI(
-        model="gemini-3-flash",  # ou "gemini-1.5-flash"
+        model="gemini-3.1-flash-lite",  # ou "gemini-1.5-flash"
         google_api_key=settings.GEMINI_API_KEY,
         temperature=0.7,
         max_tokens=None,
