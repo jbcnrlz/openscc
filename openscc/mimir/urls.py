4bd8a1e11b37c5fa3acdb473c1b223e28c3ea4d4
João Cardia <joao.cardia@fatec.sp.gov.br>
2026-07-31 15:58:55 -0300

    feat: Implement feedback request functionality for questions with specialist selection

--- openscc/mimir/urls.py
+++ openscc/mimir/urls.py
@@ -147,4 +147,5 @@ urlpatterns = [
      path('pdi/projeto/parecer/cadastrar/', views.cadastrarParecerHistorico, name='cadastrarParecerHistorico'),
      path('pdi/projeto/<int:projeto_id>/analise-preditiva/', views.analisePreditivaProjeto, name='analisePreditivaProjeto'),
      path('pbl/parte/historico/<int:historico_id>/restaurar/', views.restaurarVersaoParte, name='restaurarVersaoParte'),
+     path('pergunta/<int:pergunta_id>/feedback/solicitar/', views.solicitarFeedbackPerguntaDireto, name='solicitarFeedbackPerguntaDireto')
 ]
